26ccef6b6c0947eb318a8f690a5de5681a480f0a
mdbow22 <mdbow22@gmail.com>
2021-12-30 15:12:50 -0500

    starter files and updating boilerplate

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<main> 
+  <h1>What To-Do?</h1>
+</main>
+
+<style>
+
+  main {
+    max-width: 768px;
+    margin: 0 auto;
+  }
+
+  h1 {
+    text-align: center;
+    margin: 50px 0;
+    font-size: 4.5em;
+  }
+
+</style>
